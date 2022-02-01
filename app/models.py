3aa93fa11166b893fa242491ac2ad487c0f0094f
Rolo123y <ruhollah123@hotmail.com>
2022-02-01 05:49:03 -0500

    update to get started on user registration and form generation using FlaskForms

--- app/models.py
+++ app/models.py
@@ -1,4 +1,12 @@
+from enum import unique
+from pickle import FALSE, TRUE
 from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=FALSE, unique=TRUE)
+    email_address = db.Column(db.String(length=50), nullable=FALSE, unique=TRUE)
+    password_hash = db.Column(db.String(length=60), nullable=FALSE)
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
