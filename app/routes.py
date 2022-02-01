ac37eb544013293c9b6628c0be86833c1a940c6e
Rolo123y <ruhollah123@hotmail.com>
2022-02-01 05:51:15 -0500

    added simple input form for electricity bill. This form does not currently use FlaskForm so it will have to be remade later on to follow this new structure.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from app.models import Item
+from app.forms import RegisterForm
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home",)
 def home_page():
     return render_template('home.html')
+
+@app.route("/register")
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', register_form=form)
 
 @app.route("/FAQ")
 def FAQ_page():
@@ -16,7 +22,29 @@ def FAQ_page():
 def Profile_page(username=None):
     return render_template('Profile.html', username = username)
 
-@app.route('/renderSavings/', methods=['GET', 'POST'])
-def renderSavings():
-    dataframe = Item.query.all()
-    return render_template('home.html', savings=dataframe)
+@app.route('/renderInputs', methods=['GET', 'POST'])
+def renderInputs():
+    General_Bill_Questions = [
+        'Does your monthly bill use Time-of-Use(TOU) or Tiered?',
+        'Month of bill',
+        'Delivery Charges',
+        'Regulatory Charges',
+        'Total Electricity Cost wo h.s.t',]
+    TimeOfUse_Questions = [
+        'KWH @',
+        'Total',
+        'Off-peak',
+        'Mid-peak',
+        'High-peak',
+    ]
+    Tiered_Questions = [
+        'Number',
+        'KWH @',
+        'Total'
+    ]
+    return render_template('home.html', general_questions=General_Bill_Questions,timeofuse_questions=TimeOfUse_Questions, tiered_questions=Tiered_Questions )
+
+# @app.route('/renderResults/', methods=['GET', 'POST'])
+# def renderInputs():
+#     dataframe = Item.query.all()
+#     return render_template('home.html', savings=dataframe)
