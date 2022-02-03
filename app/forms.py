5c4c50292abb9d4f302f5a0283112f291e79a76a
Rolo123y <ruhollah123@hotmail.com>
2022-02-02 20:21:22 -0500

    generate inputs based on flask forms rather than html

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 
 class RegisterForm(FlaskForm):
     username= StringField(label='User Name')
@@ -7,3 +7,26 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password')
     password2 = PasswordField(label='Confirm Password')
     submit = SubmitField(label='Create Account')
+
+class ElectricityInputForm(FlaskForm):
+    Bill_Type = SelectField(u'Does your monthly bill use Time-of-Use(TOU) or Tiered?', choices=[('timeofuse', 'Time of Use'), ('tiered', 'Tiered')])
+    TimeofUse_Off_Peak_Value = StringField(label='TimeofUse_Off_Peak_Value')
+    TimeofUse_Off_Peak_KWH = StringField(label='TimeofUse_Off_Peak_KWH')
+    TimeofUse_Off_Peak_Total = StringField(label='TimeofUse_Off_Peak_Total')
+    TimeofUse_Mid_Peak_Value = StringField(label='TimeofUse_Mid_Peak_Value')
+    TimeofUse_Mid_Peak_KWH = StringField(label='TimeofUse_Mid_Peak_KWH')
+    TimeofUse_Mid_Peak_Total = StringField(label='TimeofUse_Mid_Peak_Total')
+    TimeofUse_On_Peak_Value = StringField(label='TimeofUse_On_Peak_Value')
+    TimeofUse_On_Peak_KWH = StringField(label='TimeofUse_On_Peak_KWH')
+    TimeofUse_On_Peak_Total = StringField(label='TimeofUse_On_Peak_Total')
+
+    Tiered_Value = StringField(label='Tiered Value')
+    Tiered_KWH = StringField(label='Tiered KWH')
+    Tiered_Total = StringField(label='Tiered Total')
+
+    Month_Of_bill = SelectField(u'Month of bill', choices=[('Time-Of-Use', 'Time of Use'), ('Tiered', 'Tiered')])
+    DeliveryCharges =  StringField(label='Delivery Charges')
+    RegulatoryCharges = StringField(label='Regulatory Charges')
+    TotalElectricityCost = StringField(label='Total Electricity Cost wo h.s.t')
+
+    submit = SubmitField(label='Next')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, request
 from app.models import Item
-from app.forms import RegisterForm
+from app.forms import RegisterForm, ElectricityInputForm
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home",)
@@ -24,25 +24,8 @@ def Profile_page(username=None):
 
 @app.route('/renderInputs', methods=['GET', 'POST'])
 def renderInputs():
-    General_Bill_Questions = [
-        'Does your monthly bill use Time-of-Use(TOU) or Tiered?',
-        'Month of bill',
-        'Delivery Charges',
-        'Regulatory Charges',
-        'Total Electricity Cost wo h.s.t',]
-    TimeOfUse_Questions = [
-        'KWH @',
-        'Total',
-        'Off-peak',
-        'Mid-peak',
-        'High-peak',
-    ]
-    Tiered_Questions = [
-        'Number',
-        'KWH @',
-        'Total'
-    ]
-    return render_template('home.html', general_questions=General_Bill_Questions,timeofuse_questions=TimeOfUse_Questions, tiered_questions=Tiered_Questions )
+    form = ElectricityInputForm()
+    return render_template('home.html',  electricity_form = form)
 
 # @app.route('/renderResults/', methods=['GET', 'POST'])
 # def renderInputs():
