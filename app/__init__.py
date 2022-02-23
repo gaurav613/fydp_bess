0ed16887de4a1ae539509f730d2f6613a491160a
Rolo123y <ruhollah123@hotmail.com>
2022-02-23 14:27:51 -0500

    trying config with heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import config
+# import config
 import os
 
 app = Flask(__name__)
-app.config.from_object('config.ProdConfig')
+# app.config.from_object('config.ProdConfig')
 
 db = SQLAlchemy(app)
 
