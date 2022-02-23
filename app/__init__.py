f3908843e149d75bcbd8d518d1c3cffdc3716269
gaurav613 <gigs612@gmail.com>
2022-02-23 13:51:53 -0500

    add key for csrf

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) 
+app.config['SECRET_KEY'] = '803hfteyuse943h12'
 db = SQLAlchemy(app)
 
 from app import routes
