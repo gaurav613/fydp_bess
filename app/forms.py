b82bc855c31657ec849f0f898dca8faae6d131b5
Rolo123y <ruhollah123@hotmail.com>
2022-02-26 16:55:18 -0500

    refactored the way the inputs display by using 2 forms. A user can click a button to show a form and there are 2 buttons to click, one for timeofuse and one for tiered.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, FloatField, MonthField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, InputRequired,ValidationError
+from wtforms import StringField, PasswordField, SubmitField, FloatField, MonthField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, InputRequired, ValidationError, NumberRange
 from app.models import User
 
 
@@ -8,40 +8,44 @@ class RegisterForm(FlaskForm):
     def validate_username(self, username_to_check):
         user = User.query.filter_by(username=username_to_check.data).first()
         if user:
-            raise ValidationError("Username already exists! Please try a different username")
+            raise ValidationError(
+                "Username already exists! Please try a different username")
 
     def validate_email_address(self, email_address_to_check):
-        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        email_address = User.query.filter_by(
+            email_address=email_address_to_check.data).first()
         if email_address:
-            raise ValidationError("Email address already exists! Please try a different email address")
+            raise ValidationError(
+                "Email address already exists! Please try a different email address")
 
-    username= StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
+    username = StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password', validators=[EqualTo("password1"), DataRequired()])
     submit = SubmitField(label='Create Account')
 
-class ElectricityInputForm(FlaskForm):
-    Bill_Type = SelectField(u'Type of electricity bill', choices=[('timeofuse', 'Time of Use'), ('tiered', 'Tiered')], validators=[DataRequired()])
-    TimeofUse_Off_Peak_Value = FloatField(label='Off Peak', validators=[])
-    TimeofUse_Off_Peak_KWH = FloatField(label='KWH', validators=[])
-    TimeofUse_Off_Peak_Total = FloatField(label='Total', validators=[])
-    TimeofUse_Mid_Peak_Value = FloatField(label='Mid Peak', validators=[])
-    TimeofUse_Mid_Peak_KWH = FloatField(label='KWH', validators=[])
-    TimeofUse_Mid_Peak_Total = FloatField(label='Total', validators=[])
-    TimeofUse_On_Peak_Value = FloatField(label='On Peak', validators=[])
-    TimeofUse_On_Peak_KWH = FloatField(label='KWH', validators=[])
-    TimeofUse_On_Peak_Total = FloatField(label='Total', validators=[])
-
-    Tiered_Value = StringField(label='Tiered Value', validators=[])
-    Tiered_KWH = StringField(label='Tiered KWH', validators=[])
-    Tiered_Total = FloatField(label='Tiered Total', validators=[])
-
+class Tiered_Form(FlaskForm):
     Month_Of_bill = MonthField(u'Month of bill', validators=[InputRequired()])
-    DeliveryCharges =  FloatField(label='Delivery Charges', validators=[InputRequired()])
-    RegulatoryCharges = FloatField(label='Regulatory Charges', validators=[InputRequired()])
-    TotalElectricityCost = FloatField(label='Total Electricity Cost wo h.s.t', validators=[InputRequired()])
+    Tiered_Value = FloatField(label='Tiered Value', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    Tiered_KWH = FloatField(label='Tiered KWH', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    Tiered_Total = FloatField(label='Tiered Total', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    DeliveryCharges = FloatField(label='Delivery Charges', validators=[InputRequired(), NumberRange(min=0,max=999999)])
+    RegulatoryCharges = FloatField(label='Regulatory Charges', validators=[InputRequired(), NumberRange(min=0,max=999999)])
+    TotalElectricityCost = FloatField(label='Total Electricity Cost wo h.s.t', validators=[InputRequired(), NumberRange(min=0,max=9999999)])
+    submit = SubmitField(label='Next', id="submit_electricity")
 
-    submit = SubmitField(label='Next')
-
-    
+class Timeofuse_Form(FlaskForm):
+    Month_Of_bill = MonthField(u'Month of bill', validators=[InputRequired()])
+    TimeofUse_Off_Peak_Value = FloatField(label='Off Peak', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_Off_Peak_KWH = FloatField(label='KWH', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_Off_Peak_Total = FloatField(label='Total', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_Mid_Peak_Value = FloatField(label='Mid Peak', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_Mid_Peak_KWH = FloatField(label='KWH', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_Mid_Peak_Total = FloatField(label='Total', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_On_Peak_Value = FloatField(label='On Peak', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_On_Peak_KWH = FloatField(label='KWH', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TimeofUse_On_Peak_Total = FloatField(label='Total', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    DeliveryCharges = FloatField(label='Delivery Charges', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    RegulatoryCharges = FloatField(label='Regulatory Charges', validators=[InputRequired(), NumberRange(min=0,max=99999)])
+    TotalElectricityCost = FloatField(label='Total Electricity Cost wo h.s.t', validators=[InputRequired(), NumberRange(min=0,max=9999999)])
+    submit = SubmitField(label='Next', id="submit_electricity")
