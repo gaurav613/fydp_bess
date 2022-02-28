c8ac8fa377b7d6518e2186ab4aeba2ec0b8fbb31
Kyle199910 <32501521+Kyle199910@users.noreply.github.com>
2022-02-27 19:29:04 -0500

    minor adjustment, and replace some of the placeholder text for homepage

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///monthlysavings.db' 
+app.config['SECRET_KEY'] = "SOMERANDOMSTRING"
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) 
 db = SQLAlchemy(app)
 
 from app import routes
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///monthlysavings.db' 
+#app.config['SECRET_KEY'] = "SOMERANDOMSTRING"
