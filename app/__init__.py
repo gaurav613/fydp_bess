09ece95e2a8ffbb7ba9f82083bbb41405e75eb0c
Kyle199910 <32501521+Kyle199910@users.noreply.github.com>
2022-03-02 13:56:50 -0500

    FAQ question adjustment and file deletion

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///monthlysavings.db' 
-app.config['SECRET_KEY'] = "SOMERANDOMSTRING"
-
+app.config.from_object('config.ProdConfig')
 db = SQLAlchemy(app)
 
 from app import routes
